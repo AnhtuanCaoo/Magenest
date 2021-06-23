97f674096a466b3504a03ae41728d6c8786994f3
nguyentung9820 <nguyensontung9820@gmail.com>
2021-06-23 08:28:17 +0700

    done

--- Movie/Block/Movie.php
+++ Movie/Block/Movie.php
@@ -2,20 +2,30 @@
 namespace Magenest\Movie\Block;
 use Magento\Framework\View\Element\Template;
 class Movie extends Template
-{
+{   
+    private $_resultCollection;
     private $_productCollectionFactory;
-
+    private $_diretorCollectionFactory;
     public function __construct(
         Template\Context $context,
         \Magenest\Movie\Model\ResourceModel\Movie\CollectionFactory $productCollectionFactory,
+        \Magenest\Movie\Model\ResourceModel\Movie\Collection $resultCollection,
+        \Magenest\Movie\Model\ResourceModel\Director\CollectionFactory $directorCollectionFactor,
         array $data = [])
     {
         parent::__construct($context, $data);
         $this->_productCollectionFactory = $productCollectionFactory;
+        $this->_resultCollection = $resultCollection;
+        $this->_diretorCollectionFactory = $directorCollectionFactor;
     }
     public function getMovies() {
-        $collection = $this->_productCollectionFactory->create();
-        
+        // $collection = $this->_diretorCollectionFactory->create()
+        // ->addFieldToSelect('name', 'director');
+        $collection = $this->_productCollectionFactory->create()
+        ->addFieldToSelect('name', 'movie')
+        ->addFieldToSelect('description')
+        ->addFieldToSelect('rating');
+        $collection->joinDirector();
         return $collection;
         }
     
--- Movie/Model/ResourceModel/Movie/Collection.php
+++ Movie/Model/ResourceModel/Movie/Collection.php
@@ -6,10 +6,19 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
 {
     protected $_idFieldName = 'movie_id';
  
+
     protected function _construct()
     {
         $this->_init('Magenest\Movie\Model\Movie', 'Magenest\Movie\Model\ResourceModel\Movie');
-        // $this->_init('Magenest\Movie\Model\Director', 'Magenest\Movie\Model\ResourceModel\Director');
     }
-    
+    public function joinDirector(){
+        $actorTable = $this->getTable('magenest_actor');
+        $actormovieTable = $this->getTable('magenest_movie_actor');
+        $directorTable = $this->getTable('magenest_director');
+        $result = $this->getSelect()
+        ->join($directorTable, 'main_table.director_id='.$directorTable.'.director_id',['director' => 'name'])
+        ->join($actormovieTable,'main_table.movie_id='.$actormovieTable.'.movie_id')
+        ->join($actorTable,$actorTable.'.actor_id='.$actormovieTable.'.actor_id');
+        return $result;
+    }
 }
